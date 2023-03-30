d550c8ded249c3ff59554adb90d246199f470dc0
hmabed <hmabed@gmail.com>
2023-03-30 08:52:02 +0200

    stable version

--- second/src/App.vue
+++ second/src/App.vue
@@ -1,9 +1,7 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import PocketBase from 'pocketbase';
+import PocketBase from 'pocketbase'
 
-const pb = new PocketBase('http://127.0.0.1:8090');
-// import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
@@ -11,7 +9,14 @@ const pb = new PocketBase('http://127.0.0.1:8090');
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div>
-      <HelloWorld msg="Welcom to you!" />
+      <!-- <HelloWorld msg="Welcom to you!" /> -->
+      <label>Login    : </label><input type="email" id="login">
+      <p></p>
+      <label>Password : </label><input type="password" id="passwd">
+      <p></p>
+      <hr>
+      <button v-on:click="register()">Register</button>
+      <button v-on:click="login()">Login</button>
     </div>
   </header>
 
@@ -20,6 +25,32 @@ const pb = new PocketBase('http://127.0.0.1:8090');
   </main>
 </template>
 
+<script>
+const pb = new PocketBase('http://127.0.0.1:8090')
+export default {
+  methods: {
+    //this method allows a new user to sign up the system. Once done, the user receives an email
+    //asking for account validation. Once the validation made the user is added to the system
+    async login() {
+      alert('je suis ici');
+      await pb.collection('users').authWithPassword(document.getElementById("login").value,
+        document.getElementById("passwd").value);
+    },
+    //this method allows the already registred user to log in the system.
+    async register() {
+      alert('je suis laba');
+      await pb.collection('users').create({
+        email: document.getElementById("login").value,
+        password: document.getElementById("passwd").value,
+        passwordConfirm: document.getElementById("passwd").value,
+        name: 'John Di',
+      });
+    }
+
+
+  }
+}
+</script>
 <style scoped>
 header {
   line-height: 1.5;
